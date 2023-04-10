60692ac613fdf31c506159a333538071d6acb334
Fatih Sayarli <mfsayarli@hotmail.com>
2023-04-10 11:25:16 +0300

    cypherOperator eklendi, fields dan neoLabel silindi

---
+++ client/src/cypherOperator.tsx
@@ -0,0 +1,19 @@
+const cypherOperator = {
+    "is": "=",
+    "is not": "!=",
+    "is greater than": ">",
+    "is less than": "<",
+    "is less than or equal to": "<=",
+    "is greater than or equal to": ">=",
+    "is blank": "null",
+    "begins with": "STARTS WITH",
+    "ends with": "ENDS WITH",
+    "contains": "CONTAINS",
+    "not contains": "NOT CONTAINS",
+    "is null": "IS NULL",
+    "is not null": "IS NOT NULL",
+    //in, not in, between, not between
+
+  };
+
+  export default cypherOperator;
